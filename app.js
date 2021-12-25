3b2b037f42aa5035f33595dfb4f29c320e36ffe2
Caty Aloi <catyaloi@gmail.com>
2021-12-25 18:38:25 -0300

    actualizando el archivo de app.js

--- app.js
+++ app.js
@@ -4,8 +4,18 @@ const app = express();
 const publicPath = path.resolve (__dirname, './public');
 app.use(express.static(publicPath));
 
-/*app.get('/', (req, res) => { res.sendFile(path.resolve (__dirname, './views/detalle_producto.html')); });*/
+app.get('/', (req, res) => {res.sendFile(path.resolve(__dirname,'./views/home.html')); });
 
-app.get('/', (req, res) => {res.sendFile(path.join(__dirname,'./views/detalle_producto.html')); });
+app.get('/detalleproducto', (req, res) => {res.sendFile(path.resolve(__dirname,'./views/detalle_producto.html')); });
 
-app.listen(3000, () => console.log('Servidor corriendo 3000'));
+app.get('/catalogoproducto', (req, res) => {res.sendFile(path.resolve(__dirname,'./views/catalogo_producto.html')); });
+
+app.get('/contacto', (req, res) => {res.sendFile(path.resolve(__dirname,'./views/form_contact.html')); });
+
+app.get('/login', (req, res) => {res.sendFile(path.resolve(__dirname,'./views/form_login.html')); });
+
+app.get('/registro', (req, res) => {res.sendFile(path.resolve(__dirname,'./views/form_register.html')); });
+
+app.get('/carrito', (req, res) => {res.sendFile(path.resolve(__dirname,'./views/cart.html')); });
+
+app.listen(3000, () => console.log('Servidor Shoes_Station corriendo'));
