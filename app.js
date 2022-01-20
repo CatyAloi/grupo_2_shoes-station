30a2469ed106d2736b67fb522441ed3e4670c035
Caty Aloi <catyaloi@gmail.com>
2022-01-19 22:15:21 -0300

    Creando formulario agregar producto y actualizando las otras paginas

--- app.js
+++ app.js
@@ -1,22 +1,33 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const bodyParser = require ('body-parser');
+
 const publicPath = path.resolve (__dirname, './public');
+app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static(publicPath));
+
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {res.sendFile(path.join(__dirname,'./views/home.html')); });
 
-app.get('/detalleproducto', (req, res) => {res.sendFile(path.join(__dirname,'./views/detalle_producto.html')); });
+app.get('/detalleproducto', (req, res) => {res.sendFile(path.join(__dirname,'./views/productDetail.html')); });
 
 // app.get('/catalogoproducto', (req, res) => {res.sendFile(path.join(__dirname,'./views/catalogo_producto.html')); });
 
 app.get('/contacto', (req, res) => {res.sendFile(path.resolve(__dirname,'./views/form_contact.html')); });
 
+app.post('/contacto', (req,res) =>{
+    return res.send(req.body)
+})
+
 // app.get('/login', (req, res) => {res.sendFile(path.join(__dirname,'./views/form_login.html')); });
 
 // app.get('/registro', (req, res) => {res.sendFile(path.join(__dirname,'./views/form_register.html')); });
 
-app.get('/carrito', (req, res) => {res.sendFile(path.resolve(__dirname,'./views/carrito.html')); });
+app.get('/carrito', (req, res) => {res.sendFile(path.resolve(__dirname,'./views/productCart.html')); });
+
+app.get('/agregarproductos', (req, res) => {res.sendFile(path.resolve(__dirname,'./views/addProduct.html')); });
 
 app.get('/editarproductos', (req, res) => {res.sendFile(path.resolve(__dirname,'./views/form_edition.html')); });
 
