8bf13a559447e4947c03335e2ce070dbe84dab6c
Caty Aloi <catyaloi@gmail.com>
2022-02-15 18:56:39 -0300

    Funcionando el botón Agregar Producto

--- controllers/productControllers.js
+++ controllers/productControllers.js
@@ -1,3 +1,10 @@
+
+const fs = require('fs');
+const path = require('path');
+
+const productsFilePath = path.join(__dirname, '../data/productsData.json');
+const productsJson = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
+
 const listaProductos = require('../models/data');
 
 const product_Controllers = {
@@ -11,12 +18,29 @@ const product_Controllers = {
     carrito: (req, res)=> {
         res.render('products/productCart');
     },
-    crearProducto: (req, res)=> {
-        res.render('products/addProduct');
+
+    // MUESTRA EL FORMULARIO DE AGREGAR NUEVO PRODUCTO
+    crearProducto: (req,res)=> { 
+        res.render('products/addProduct'); },
+
+    //GUARDAR O AGREGAR EL NUEVO PRODUCTO A LA BD       
+    store: (req,res)=> { 
+
+        let productNew = (req.body);
+        productNew.img = req.file.filename;
+        productsJson.push(productNew)
+        const productNewJson = JSON.stringify(productsJson, null, 2);
+        fs.writeFileSync('./data/productsData.json', productNewJson);
+        res.redirect('/catalogo'); 
     },
+    
+    //EDITA EL PRODUCTO EXISTENTE          
     editarProducto: (req, res)=> {
         res.render('products/form_edition');
     },
+
+    //ELIMINA EL PRODUCTO EXISTENTE 
+
 };
 
 module.exports = product_Controllers;
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,13 +1,43 @@
 const express = require('express');
 const path = require('path');
 const router = express.Router();
+const multer = require('multer');
+
+const storage = multer.diskStorage({
+    // Carpeta destino del archivo
+    destination: function (req, file, cb) {
+        cb(null, path.resolve('public/images'));
+    },
+    filename: function (req, file, cb) {
+        // Nombre del archivo
+        cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname))
+    }
+});
+
+//Crear una instancia de multer con esa lógica
+// clase( parametros )
+const upload = multer({ storage });
+
 
 const controladorProduct = require('../controllers/productControllers');
 
+//SHOW ALL PRODUCTS//
 router.get('/catalogo', controladorProduct.catalogo);
-router.get('/detalle/:id', controladorProduct.detalle);
+
+//CART//
 router.get('/carrito', controladorProduct.carrito);
+
+//GET ONE PRODUCT//
+router.get('/detalle/:id', controladorProduct.detalle);
+
+//CREATE ONE PRODUCT//
 router.get('/crearproductos', controladorProduct.crearProducto);
+router.post('/crearproductos', upload.single('img'), controladorProduct.store);
+
+//EDIT ONE PRODUCT//
 router.get('/editarproductos', controladorProduct.editarProducto);
+
+//DELETE ONE PRODUCT//
+
 
 module.exports = router;
