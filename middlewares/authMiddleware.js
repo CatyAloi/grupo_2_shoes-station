a3906b3b3be32e81a479dc9e8862e029622f0731
Caty Aloi <catyaloi@gmail.com>
2022-04-06 18:16:25 -0300

    actualizando branch

--- middlewares/authMiddleware.js
+++
@@ -1,9 +0,0 @@
-function authMiddleware(req, res, next){
-    if(req.session.userLogged!=undefined){
-        next();
-    } else {
-        res.send('Esta página es sólo para usuarios');
-    }
-    };
-    
-    module.exports = authtMiddleware;
